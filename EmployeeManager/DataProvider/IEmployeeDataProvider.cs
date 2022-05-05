991f482e5c1fc51dc0f3b3475a572abe070998c9
Stefan Buzoianu <stefan.buzoianu@napa.fi>
2022-05-05 21:25:59 +0300

    Add Data Provider classes

---
+++ EmployeeManager/DataProvider/IEmployeeDataProvider.cs
@@ -0,0 +1,14 @@
+﻿using EmployeeManager.Model;
+using System.Collections.Generic;
+
+namespace EmployeeManager.DataProvider
+{
+  public interface IEmployeeDataProvider
+  {
+    IEnumerable<Employee> LoadEmployees();
+
+    void SaveEmployee(Employee employee);
+
+    IEnumerable<JobRole> LoadJobRoles();
+  }
+}
